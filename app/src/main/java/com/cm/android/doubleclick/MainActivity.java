a8d925c8a758e5675244ddaa8fe3bff8e752106e
wusm <wusm@cmft.com>
2019-05-14 15:04:42 +0800

    init project

---
+++ app/src/main/java/com/cm/android/doubleclick/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cm.android.doubleclick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
