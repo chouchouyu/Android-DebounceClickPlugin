5e7f32460c5fadb0bfe0459aed6e4d80b17230cd
吴思敏 <wusm@cmrh.com>
2019-05-25 09:56:10 +0800

    clean bean package codes

--- buildSrc/src/main/groovy/com/github/susan/clickdebounce/plugin/bean/TracedClass.groovy
+++ buildSrc/src/main/groovy/com/github/susan/clickdebounce/plugin/bean/TracedClass.groovy
@@ -1,31 +1,28 @@
 package com.github.susan.clickdebounce.plugin.bean;
 
-import java.io.Serializable;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
-public class TracedClass implements Serializable {
+class TracedClass implements Serializable {
 
-    private String className;
-    private Set<String> tracedMethods = new LinkedHashSet<>();
+    private String className
+    private Set<String> tracedMethods = new LinkedHashSet<>()
 
     TracedClass(String className) {
-        this.className = className;
+        this.className = className
     }
 
     void addTracedMethod(String methodSignature) {
-        tracedMethods.add(methodSignature);
+        tracedMethods.add(methodSignature)
     }
 
-    public String getClassName() {
-        return className;
+    String getClassName() {
+        return className
     }
 
-    public Set<String> getTracedMethods() {
-        return tracedMethods;
+    Set<String> getTracedMethods() {
+        return tracedMethods
     }
 
-    public boolean hasTracedMethod() {
-        return tracedMethods.size() > 0;
+    boolean hasTracedMethod() {
+        return tracedMethods.size() > 0
     }
 }
